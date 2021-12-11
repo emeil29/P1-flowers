467ebf11b16cba5096ed43f568f21f9b45d509eb
emeil29 <76056703+emeil29@users.noreply.github.com>
2021-12-11 10:27:21 -0800

    Add files via upload

---
+++ P1.js
@@ -0,0 +1,18 @@
+// " use strict "
+const panels = document.querySelectorAll('.panel')
+
+
+const removeActive = function(){
+    panels.forEach (panel =>{
+     panel.classList.remove('active')
+    })
+}
+
+panels.forEach(panel =>{
+    panel.addEventListener('click', function(){
+        removeActive()
+        panel.classList.add('active')  
+         
+    })   
+})
+
